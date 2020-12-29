1dbc7f3a55315b6fd0944e377aaef6b7bcafcac5
Remi Rampin <r@remirampin.com>
2020-12-29 12:21:29 -0500

    Get executable name using xdotool and /proc

--- pctracker/x11.py
+++ pctracker/x11.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import typing
 
@@ -19,12 +20,29 @@ def _get_active_window():
     return _parse_number(output.decode('utf-8').strip())
 
 
-def _get_window_name(window_id):
+def _get_window_name_exe(window_id):
     try:
-        output = subprocess.check_output(['xdotool', 'getwindowname', str(window_id)])
+        output = subprocess.check_output([
+            'xdotool',
+            'getwindowname', str(window_id),
+            'getwindowpid', str(window_id),
+        ])
     except subprocess.CalledProcessError:
         raise ValueError
-    return output.decode('utf-8').rstrip('\r\n')
+    output = output.decode('utf-8').rstrip('\r\n')
+
+    if '\n' in output:
+        name, pid = output.rsplit('\n', 1)
+        pid = int(pid)
+        try:
+            executable = os.readlink('/proc/{pid}/exe'.format(pid=pid))
+        except FileNotFoundError:
+            raise ValueError
+    else:
+        name = output
+        executable = None
+
+    return name, executable
 
 
 def get_windows() -> typing.List[Window]:
@@ -43,11 +61,11 @@ def get_windows() -> typing.List[Window]:
             windows = []
             for id in ids:
                 try:
-                    name = _get_window_name(id)
+                    name, executable = _get_window_name_exe(id)
                 except ValueError:
                     # Window was gone before we could get its name, that's fine
                     continue
-                windows.append(Window(id, name, active_id == id))
+                windows.append(Window(id, name, active_id == id, executable))
             return windows
 
     raise RuntimeError("xprop didn't provide active windows")
